0b5620b1d910f05b104f17c5b5e816f7a60bdd6b
ivanivanov9 <72138590+ivanivanov9@users.noreply.github.com>
2021-03-10 11:31:39 +0200

    Add files via upload

--- GenericStack2.java
+++ GenericStack2.java
@@ -1,10 +1,36 @@
 package generics;
+import java.util.ArrayList;
+public class GenericStack2<E> extends ArrayList<E>{
+	private java.util.ArrayList <E> list = new java.util.ArrayList<>();
 
-public class GenericStack2 {
+	public GenericStack2() {
+		
+	}
+	
+	//public int getSize() {
+		//return list.size();
+	//}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public E peek() {
+		return get(size() - 1);
 	}
 
+	public void push(E o) {
+		add(o);
+	}
+
+	public E pop() {
+		E o = get(size() - 1);
+		remove(size() - 1);
+		return o;
+	}
+
+	public boolean isEmpty() {
+		return super.isEmpty();
+	}
+
+	@Override
+	public String toString() {
+		return "stack: " + super.toString();
+	}
 }
